f032080dd2f87e51af2cd609e5d693960bdfd094
TobiH-GE <tobias.hartwich@hotmail.de>
2021-02-20 09:04:25 +0100

    comments

--- NetworkModel/Model.cs
+++ NetworkModel/Model.cs
@@ -32,70 +32,72 @@ namespace NetworkModel
             int receivedBytes = 0;
             while (connection.Connected)
             {
-                    receivedData = new byte[1024];
+                receivedData = new byte[1024];
                 receivedDataDebug = new byte[1024]; // debug
 
                 receivedBytes = await connection.GetStream().ReadAsync(receivedData.AsMemory(0, receivedData.Length), cts.Token);
                 receivedDataDebug = receivedData;
-                    int MessageSize = BitConverter.ToInt32(receivedData, 4);
+                int MessageSize = BitConverter.ToInt32(receivedData, 4);
 
-                    OnReceive.Invoke("Status", $"received data with receivedBytes {receivedBytes} MessageSize {MessageSize} ...");
-                    do // TODO: remove bug
+                //OnReceive.Invoke("Status", $"received data with receivedBytes {receivedBytes} MessageSize {MessageSize} ...");
+                do
+                {
+                    byte[] data = receivedData[..MessageSize];
+                    MsgType MessageType = (MsgType)data[0];
+                    SubType SubType = (SubType)data[1];
+
+                    if (MessageType == MsgType.Command)
                     {
-                        byte[] data = receivedData[..MessageSize];
-                        MsgType MessageType = (MsgType)data[0];
-                        SubType SubType = (SubType)data[1];
-
-                        if (MessageType == MsgType.Command)
+                        MessageCommand incomingMessage = new MessageCommand(data);
+                        if (SubType == SubType.LoginRequest)
                         {
-                            MessageCommand incomingMessage = new MessageCommand(data);
-                            if (SubType == SubType.LoginRequest)
-                            {
-                                OnReceive.Invoke("Status", "server is requesting login data ...");
-                                Send(new MessageCommand(MsgType.Command, SubType.Login, "password", Username));
-                                // do something
-                            }
-                            else if (SubType == SubType.JoinOk)
-                            {
-                                OnReceive.Invoke("Status", $"joining room {incomingMessage.Parameter}.");
-                                OnJoinOk.Invoke(incomingMessage.Parameter);
-                            }
-                            else if (SubType == SubType.LeaveOk)
-                            {
-                                OnReceive.Invoke("Status", $"leaving room {incomingMessage.Parameter}.");
-                                OnLeaveOk.Invoke(incomingMessage.Parameter);
-                            }
-                            else if (SubType == SubType.Userlist)
-                            {
-                                OnReceive.Invoke("Status", $"received userlist room: {incomingMessage.Parameter}.");
-                            }
-                        }
-                        else if (MessageType == MsgType.Data)
-                        {
-                            MessageData incomingMessage = new MessageData(data);
+                            OnReceive.Invoke("Status", "server is requesting login data ...");
+                            Send(new MessageCommand(MsgType.Command, SubType.Login, "password", Username));
                             // do something
                         }
-                        else if (MessageType == MsgType.Text)
+                        else if (SubType == SubType.JoinOk)
                         {
-                            MessageText incomingMessage = new MessageText(data);
-
-                            if (SubType == SubType.Room)
-                            {
-                                OnReceive.Invoke(incomingMessage.Parameter, incomingMessage.Username + ": " + incomingMessage.Text);
-                            }
-                            else
-                            {
-                                OnReceive.Invoke("Status", incomingMessage.Username + ": " + incomingMessage.Text);
-                            }
+                            OnReceive.Invoke("Status", $"joining room {incomingMessage.Parameter}.");
+                            OnJoinOk.Invoke(incomingMessage.Parameter);
                         }
-                    if (receivedBytes > MessageSize + 8)
+                        else if (SubType == SubType.LeaveOk)
+                        {
+                            OnReceive.Invoke("Status", $"leaving room {incomingMessage.Parameter}.");
+                            OnLeaveOk.Invoke(incomingMessage.Parameter);
+                        }
+                        else if (SubType == SubType.Userlist)
+                        {
+                            OnReceive.Invoke("Status", $"received userlist room: {incomingMessage.Parameter}.");
+                        }
+                    }
+                    else if (MessageType == MsgType.Data)
+                    {
+                        MessageData incomingMessage = new MessageData(data);
+                        // do something
+                    }
+                    else if (MessageType == MsgType.Text)
+                    {
+                        MessageText incomingMessage = new MessageText(data);
+
+                        if (SubType == SubType.Room)
+                        {
+                            OnReceive.Invoke(incomingMessage.Parameter, incomingMessage.Username + ": " + incomingMessage.Text);
+                        }
+                        else
+                        {
+                            OnReceive.Invoke("Status", incomingMessage.Username + ": " + incomingMessage.Text);
+                        }
+                    }
+                    if (receivedBytes > MessageSize + 8) // additional message with minimum message size in buffer, check if valid
                     {
                         receivedData = receivedData[MessageSize..];
                         MessageSize = BitConverter.ToInt32(receivedData, 4);
                     }
                     else
+                    {
                         MessageSize = 0;
-                    } while (MessageSize > 0);
+                    }
+                } while (MessageSize > 0);
                 
                 if (receivedBytes < 1) // TODO: optimize, remove bug
                 {
